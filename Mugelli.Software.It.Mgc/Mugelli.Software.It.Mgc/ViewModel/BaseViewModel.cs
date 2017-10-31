b606df11ac9bad48ad6b4849020d34953c41e598
Giovanni Mugelli <giovanni.mugelli@simeyoung.it>
2017-10-31 16:20:29 +0100

    add render an basemodel and baseviewmodel

---
+++ Mugelli.Software.It.Mgc/Mugelli.Software.It.Mgc/ViewModel/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Mugelli.Software.It.Mgc.ViewModel
+{
+    public abstract class BaseViewModel : ViewModelBase
+    {
+
+    }
+}
