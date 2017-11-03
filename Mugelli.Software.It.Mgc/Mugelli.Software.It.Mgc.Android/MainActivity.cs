3c29ba2b9f2489e62e5c8ca3219ba85259d6db25
Giovanni Mugelli <giovanni.mugelli@simeyoung.it>
2017-11-03 18:37:35 +0100

    aggiunto firebase notification

--- Mugelli.Software.It.Mgc/Mugelli.Software.It.Mgc.Android/MainActivity.cs
+++ Mugelli.Software.It.Mgc/Mugelli.Software.It.Mgc.Android/MainActivity.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace Mugelli.Software.It.Mgc.Droid
 {
@@ -21,7 +23,55 @@ namespace Mugelli.Software.It.Mgc.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            FirebasePushNotificationManager.ProcessIntent(Intent);
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this,true);
+#else
+            FirebasePushNotificationManager.Initialize(this, false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Received");
+                //SendNotification(p.Data["body"]);
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Opened");
+                foreach (var data in p.Data)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                }
+
+                if (!string.IsNullOrEmpty(p.Identifier))
+                {
+                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
+                }
+
+            };
         }
+
+        //private void SendNotification(string messageBody)
+        //{
+        //    var intent = new Intent(this, typeof(MainActivity));
+        //    intent.AddFlags(ActivityFlags.ClearTop);
+        //    var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+        //    var notificationBuilder = new Notification.Builder(this)
+        //        .SetSmallIcon(Resource.Drawable.icon)
+        //        .SetContentTitle("FCM Message")
+        //        .SetContentText(messageBody)
+        //        .SetAutoCancel(true)
+        //        .SetContentIntent(pendingIntent);
+
+        //    var notificationManager = NotificationManager.FromContext(this);
+        //    notificationManager.Notify(0, notificationBuilder.Build());
+        //}
     }
 }
 
