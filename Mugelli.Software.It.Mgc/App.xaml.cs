392d9056658d5e08f2e277a267b6a2c5e8e7cc23
Giovanni Mugelli <giovanni.mugelli@simeyoung.it>
2018-03-22 11:38:45 +0100

    aggiornasto app.cs

--- Mugelli.Software.It.Mgc/App.xaml.cs
+++ Mugelli.Software.It.Mgc/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Mugelli.Software.It.Mgc.Converters;
 using Mugelli.Software.It.Mgc.Navigations;
 using Mugelli.Software.It.Mgc.ViewModel;
 using Xamarin.Forms;
@@ -54,21 +55,129 @@ namespace Mugelli.Software.It.Mgc
             {
                 Resources.Add("ViewModelLocator", new ViewModelLocator());
             }
-            if (!Resources.ContainsKey("MainAccentColor"))
+            if (!Resources.ContainsKey("SelectedItemEventArgsToSelectedItemConverter"))
             {
-                Resources.Add("MainAccentColor", Color.FromHex("#1e73be"));
+                Resources.Add("SelectedItemEventArgsToSelectedItemConverter", new SelectedItemEventArgsToSelectedItemConverter());
             }
-            if (!Resources.ContainsKey("LightAccentColor"))
+            if (!Resources.ContainsKey("InverseBoolConverter"))
             {
-                Resources.Add("LightAccentColor", Color.FromHex("#61a1f1"));
+                Resources.Add("InverseBoolConverter", new InverseBoolConverter());
             }
-            if (!Resources.ContainsKey("DarkAccentColor"))
+            if (!Resources.ContainsKey("IsEmptyConverter"))
             {
-                Resources.Add("DarkAccentColor", Color.FromHex("#00488d"));
+                Resources.Add("IsEmptyConverter", new IsEmptyConverter());
             }
-            if (!Resources.ContainsKey("MainBackgroundColor"))
+            if (!Resources.ContainsKey("IsNotNullToBoolConverter"))
             {
-                Resources.Add("MainBackgroundColor", Color.FromHex("#f4f4f4"));
+                Resources.Add("IsNotNullToBoolConverter", new IsNotNullToBoolConverter());
+            }
+            if (!Resources.ContainsKey("IsNullToBoolConverter"))
+            {
+                Resources.Add("IsNullToBoolConverter", new IsNullToBoolConverter());
+            }
+            if (!Resources.ContainsKey("NullIntValueConverter"))
+            {
+                Resources.Add("NullIntValueConverter", new NullIntValueConverter());
+            }
+            if (!Resources.ContainsKey("TypeEventColorConverter"))
+            {
+                Resources.Add("TypeEventColorConverter", new TypeEventColorConverter());
+            }
+            if (!Resources.ContainsKey("TypeEventImageConverter"))
+            {
+                Resources.Add("TypeEventImageConverter", new TypeEventImageConverter());
+            }
+            if (!Resources.ContainsKey("TypeEventTextConverter"))
+            {
+                Resources.Add("TypeEventTextConverter", new TypeEventTextConverter());
+            }
+            if (!Resources.ContainsKey("NotBooleanConverter"))
+            {
+                Resources.Add("NotBooleanConverter", new NotBooleanConverter());
+            }
+
+
+
+
+            if (!Resources.ContainsKey("PrimaryTextColor"))
+            {
+                Resources.Add("PrimaryTextColor", Color.FromHex("#333"));
+            }
+            if (!Resources.ContainsKey("SecondaryTextColor"))
+            {
+                Resources.Add("SecondaryTextColor", Color.FromHex("#FFFFFF"));
+            }
+            if (!Resources.ContainsKey("MgcColor"))
+            {
+                Resources.Add("MgcColor", Color.FromHex("#63388a"));
+            }
+            if (!Resources.ContainsKey("AsphaltPrimary"))
+            {
+                Resources.Add("AsphaltPrimary", Color.FromHex("#5c7d90"));
+            }
+            if (!Resources.ContainsKey("RedPrimary"))
+            {
+                Resources.Add("RedPrimary", Color.FromHex("#F56D50"));
+            }
+            if (!Resources.ContainsKey("GrayUltraLight"))
+            {
+                Resources.Add("GrayUltraLight", Color.FromHex("#FFFFFF"));
+            }
+            if (!Resources.ContainsKey("GrayPrimary"))
+            {
+                Resources.Add("GrayPrimary", Color.FromHex("#B9B9B9"));
+            }
+            if (!Resources.ContainsKey("GrayLight"))
+            {
+                Resources.Add("GrayLight", Color.FromHex("#F2F2F2"));
+            }
+            if (!Resources.ContainsKey("GrayDark"))
+            {
+                Resources.Add("GrayDark", Color.FromHex("#959595"));
+            }
+            if (!Resources.ContainsKey("GrayMedium"))
+            {
+                Resources.Add("GrayMedium", Color.FromHex("#B9B9B9"));
+            }
+
+
+            if (!Resources.ContainsKey("TitleFont"))
+            {
+                Resources.Add("TitleFont", (double)20);
+            }
+            if (!Resources.ContainsKey("HandleFont"))
+            {
+                Resources.Add("HandleFont", (double)12);
+            }
+            if (!Resources.ContainsKey("BodyFont"))
+            {
+                Resources.Add("BodyFont", (double)12);
+            }
+            if (!Resources.ContainsKey("HeaderFont"))
+            {
+                Resources.Add("HeaderFont", (double)30);
+            }
+            if (!Resources.ContainsKey("SubHeaderFont"))
+            {
+                Resources.Add("SubHeaderFont", (double)18);
+            }
+            if (!Resources.ContainsKey("TitleMediumFont"))
+            {
+                Resources.Add("TitleMediumFont", (double)20);
+            }
+            if (!Resources.ContainsKey("BodyMediumFont"))
+            {
+                Resources.Add("BodyMediumFont", (double)18);
+            }
+
+
+            if (!Resources.ContainsKey("RegularFontFamily"))
+            {
+                Resources.Add("RegularFontFamily", new
+                {
+                    Android = "sans-serif",
+                    iOS = "HelveticaNeue"
+                });
             }
         }
     }
