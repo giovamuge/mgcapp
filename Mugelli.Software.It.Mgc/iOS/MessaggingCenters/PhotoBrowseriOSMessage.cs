1b72d39772155163b9bb1e9a8b2dea86b9e1c1f3
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2018-03-26 15:33:22 +0200

    aggiornato icon ios

--- Mugelli.Software.It.Mgc/iOS/MessaggingCenters/PhotoBrowseriOSMessage.cs
+++ Mugelli.Software.It.Mgc/iOS/MessaggingCenters/PhotoBrowseriOSMessage.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using Mugelli.Software.It.Mgc.MessagingCenters;
+using Ricardo.LibMWPhotoBrowser.iOS;
 using Stormlion.PhotoBrowser;
+using UIKit;
 using Xamarin.Forms;
 
 namespace Mugelli.Software.It.Mgc.iOS.MessaggingCenters
@@ -14,7 +16,26 @@ namespace Mugelli.Software.It.Mgc.iOS.MessaggingCenters
             MessagingCenter.Subscribe<BrowserPhotosMessage>(app, nameof(BrowserPhotosMessage), (sender) =>
             {
                 var images = sender?.Images;
-                var photos = images.Select(x => new Photo { URL = x }).ToList();
+                //var photos = images.Select(x => new Photo { URL = x }).ToList();
+                var ph = new List<Photo>
+                    {
+                        new Photo
+                        {
+                            URL = "http://www.sime.it/it/media/images/homepage_top/banner_simeprofessional.jpg",
+                            Title = "Vincent"
+                        },
+                        new Photo
+                        {
+                            URL = "https://raw.githubusercontent.com/stfalcon-studio/FrescoImageViewer/v.0.5.0/images/posters/Jules.jpg",
+                            Title = "Jules"
+                        },
+                        new Photo
+                        {
+                            URL = "https://raw.githubusercontent.com/stfalcon-studio/FrescoImageViewer/v.0.5.0/images/posters/Korben.jpg",
+                            Title = "Korben"
+                    }
+                };
+                var photos = ph.ToList();
                 var pb = new PhotoBrowser
                 {
                     Photos = photos,
@@ -23,6 +44,11 @@ namespace Mugelli.Software.It.Mgc.iOS.MessaggingCenters
                         //Debug.WriteLine($"Clicked {index}");
                     }
                 };
+
+
+                var _mainBrowser = new MyMWPhotoBrower(pb);
+                _mainBrowser.Show();
+                //pb.Show();
 
                 //var pb = new PhotoBrowser
                 //{
@@ -50,8 +76,85 @@ namespace Mugelli.Software.It.Mgc.iOS.MessaggingCenters
                 //    }
                 //};
 
-                pb.Show();
             });
+        }
+
+        public class MyMWPhotoBrower : MWPhotoBrowserDelegate
+        {
+            protected PhotoBrowser _photoBrowser;
+
+            protected List<MWPhoto> _photos = new List<MWPhoto>();
+
+            public MyMWPhotoBrower(PhotoBrowser photoBrowser)
+            {
+                _photoBrowser = photoBrowser;
+            }
+
+            public void Show()
+            {
+                _photos = new List<MWPhoto>();
+
+                foreach (Photo p in _photoBrowser.Photos)
+                {
+                    MWPhoto mp = MWPhoto.FromUrl(new Foundation.NSUrl(p.URL));
+
+                    if (!string.IsNullOrWhiteSpace(p.Title))
+                    {
+                        mp.Caption = p.Title;
+                    }
+
+                    _photos.Add(mp);
+                }
+
+                MWPhotoBrowser browser = new MWPhotoBrowser(this);
+
+                browser.DisplayActionButton = _photoBrowser.ActionButtonPressed != null;
+                browser.SetCurrentPhoto((nuint)_photoBrowser.StartIndex);
+
+                UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(new UINavigationController(browser), true, null);
+            }
+
+            public override MWPhoto GetPhoto(MWPhotoBrowser photoBrowser, nuint index) => _photos[(int)index];
+
+            public override nuint NumberOfPhotosInPhotoBrowser(MWPhotoBrowser photoBrowser) => (nuint)_photos.Count;
+
+            public override void OnActionButtonPressed(MWPhotoBrowser photoBrowser, nuint index)
+            {
+                _photoBrowser.ActionButtonPressed?.Invoke((int)index);
+            }
+
+            public void Close()
+            {
+                UIApplication.SharedApplication.KeyWindow.RootViewController.DismissViewController(true, null);
+            }
+        }
+
+        public class PhotoBrowserImplementation : IPhotoBrowser
+        {
+            protected static MyMWPhotoBrower _mainBrowser;
+
+            public void Show(PhotoBrowser photoBrowser)
+            {
+                _mainBrowser = new MyMWPhotoBrower(photoBrowser);
+                _mainBrowser.Show();
+            }
+
+            public void Close()
+            {
+                if (_mainBrowser != null)
+                {
+                    _mainBrowser.Close();
+                    _mainBrowser = null;
+                }
+            }
+        }
+
+        public class Platform
+        {
+            public static void Init()
+            {
+                DependencyService.Register<PhotoBrowserImplementation>();
+            }
         }
     }
 }
