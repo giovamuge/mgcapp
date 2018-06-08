763022287c15e2646ead1cb3b5f5edea6b3cd4a5
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2018-06-08 17:54:27 +0200

    aggiunto nuovo fcm push notification (non ancora funzionante) error: firebase dice che il token non è registrato

--- Mugelli.Software.It.Mgc/Droid/MainActivity.cs
+++ Mugelli.Software.It.Mgc/Droid/MainActivity.cs
@@ -7,7 +7,9 @@ using FFImageLoading;
 using CarouselView.FormsPlugin.Android;
 using FFImageLoading.Forms.Droid;
 using Mugelli.Software.It.Mgc.Droid.MessagingCenters;
-using Plugin.FirebasePushNotification;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Iid;
 
 namespace Mugelli.Software.It.Mgc.Droid
 {
@@ -23,16 +25,16 @@ namespace Mugelli.Software.It.Mgc.Droid
 
             CachedImageRenderer.Init(true);
 
-			var config = new FFImageLoading.Config.Configuration
-			{
-				VerboseLogging = false,
-				VerbosePerformanceLogging = false,
-				VerboseMemoryCacheLogging = false,
-				VerboseLoadingCancelledLogging = false,
-				DiskCacheDuration = TimeSpan.FromDays(30),
-				FadeAnimationEnabled = true,
-				FadeAnimationForCachedImages = false,
-				AllowUpscale = true
+            var config = new FFImageLoading.Config.Configuration
+            {
+                VerboseLogging = false,
+                VerbosePerformanceLogging = false,
+                VerboseMemoryCacheLogging = false,
+                VerboseLoadingCancelledLogging = false,
+                DiskCacheDuration = TimeSpan.FromDays(30),
+                FadeAnimationEnabled = true,
+                FadeAnimationForCachedImages = false,
+                AllowUpscale = true
             };
             ImageService.Instance.Initialize(config);
 
@@ -112,29 +114,53 @@ namespace Mugelli.Software.It.Mgc.Droid
 
 
 
-			//If debug you should reset the token each time.
-#if DEBUG
-            FirebasePushNotificationManager.Initialize(this,true);
-#else
-            FirebasePushNotificationManager.Initialize(this, false);
-#endif
+            //If debug you should reset the token each time.
+            //#if DEBUG
+            //            FirebasePushNotificationManager.Initialize(this,true);
+            //#else
+            //            FirebasePushNotificationManager.Initialize(this, false);
+            //#endif
 
-            //Handle notification when app is closed here
-            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            ////Handle notification when app is closed here
+            //CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            //{
+
+
+            //};
+
+            ////Set the default notification channel for your app when running Android Oreo
+            //if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            //{
+            //    //Change for your default notification channel id here
+            //    FirebasePushNotificationManager.DefaultNotificationChannelId = "DefaultChannel";
+
+            //    //Change for your default notification channel name here
+            //    FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            //}
+
+            if (!IsPlayServicesAvailable())  return;
+            //Firebase.Iid.FirebaseInstanceId.Instance.GetToken();
+            Firebase.Messaging.FirebaseMessaging.Instance.SubscribeToTopic("testt");
+            Log.Debug("Token", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
             {
-
-
-            };
-
-            //Set the default notification channel for your app when running Android Oreo
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                //Change for your default notification channel id here
-                FirebasePushNotificationManager.DefaultNotificationChannelId = "DefaultChannel";
-
-                //Change for your default notification channel name here
-                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Error(nameof(MainActivity), GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(nameof(MainActivity), "This device is not supported");
+                    Finish();
+                }
+                return false;
             }
+
+            Log.Debug(nameof(MainActivity), "Google Play Services is available.");
+            return true;
         }
     }
 }
---
+++ Mugelli.Software.It.Mgc/Droid/Services/FirebaseIiDService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Support.V4.App;
+using Android.Util;
+using Firebase.Iid;
+using Firebase.Messaging;
+
+namespace Mugelli.Software.It.Mgc.Droid.Services
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class FirebaseIiDService : FirebaseInstanceIdService
+    {
+        const string TAG = nameof(FirebaseIiDService);
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+            // Add custom implementation, as needed.
+        }
+    }
+
+
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+        }
+
+        void SendNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0 /* Request code */, intent, PendingIntentFlags.OneShot);
+
+            var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                .SetContentTitle("FCM Message")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetSound(defaultSoundUri)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
